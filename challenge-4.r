8df6b201108bf044ddba7a65edc0ffb96f9b3d54
Your Name <Alvin Aziz>
2021-04-13 17:52:18 +0200

    added answers, need fixing

--- challenge-4.r
+++ challenge-4.r
@@ -1,3 +1,5 @@
+### --- R Challange 4, Alvin Aziz, 13.04.2021 --- ###
+
 # P. Kessling
 # Challenge 5: Datensatz wiederzusammenfrickeln
 # Wir haben einen Datensatz aus der Wikipedia geladen und ihn als JSON abgelegt.
@@ -8,10 +10,53 @@
 # * Berechne das durchschnittliche Alter für die einzelnen Fraktionen und sortiere den Datensatz nach dieser Variable.
 
 library(assertthat)
+library(dplyr)
+library(readr)
+library(purrr)
+library(tidyr)
+library(jsonlite)
 
-# data <- …
-# ans1 <- …
+### --- Load JSON data --- ### delete "as tibble" to revert
+parteien <- list.files("./data/", full.names = T) %>% 
+  map_dfr(fromJSON) %>% 
+  as_tibble(
+    key = c("abgeordnete", "fraktion"),
+    tibble = (list(
+      tibble(
+        value = c(partei)
+      ),
+      tibble(
+        value = c(abgeordnete)
+      )
+    ))
+  )
 
+### prev ver.
+parteien <- list.files("./data/", full.names = T) %>% 
+  map_dfr(fromJSON) %>% 
+  as_tibble()
+
+colnames(parteien)[2] <- "partei"
+
+### --- Unnest dataset --- ###
+bt2 <- parteien %>% 
+  unnest(partei) %>% 
+  unnest(abgeordnete)
+  
+# testing unnest, delete to revert
+bt2 <- parteien %>% 
+  unnest(c(partei, abgeordnete))
+
+
+### --- Average political party age --- ###
+ans1 <- bt2 %>% 
+  mutate(age = 2021 - lebensdaten) %>% 
+  group_by(partei) %>% 
+  summarise(mean(age)) %>% 
+  arrange(desc(`mean(age)`))
+
+colnames(ans1)[2] <- "avg_age"
+  
 if (
   assert_that(
     openssl::md5(paste(map_chr(bt2, paste, collapse = "") , collapse = "")) == "926c50623af03708fa768e0003cc18c6"
